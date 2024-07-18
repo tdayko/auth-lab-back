ab425c2bac18d23bf4ae56e3fb1ca69b59e0224e
urltapas <ryan.bromati@hotmail.com>
2024-07-18 19:07:25 -0300

    refactor: add email and username uniqueness constraints to user entity

--- src/AuthLab.Infra/DbContext/AuthLabDbContext.cs
+++ src/AuthLab.Infra/DbContext/AuthLabDbContext.cs
@@ -10,12 +10,15 @@ public class AuthLabDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseInMemoryDatabase(databaseName: "AuthLabDb");
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasKey(x => x.Id);
         modelBuilder.Entity<User>().Property(x => x.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<User>().Property(x => x.Email).IsRequired();
+        modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique();
         modelBuilder.Entity<User>().Property(x => x.Username).IsRequired();
+        modelBuilder.Entity<User>().HasIndex(x => x.Username).IsUnique();
         modelBuilder.Entity<User>().Property(x => x.Password).IsRequired();
     }
 }
