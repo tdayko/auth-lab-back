b20a1a9f1b679cb2c583be8789160c6465e8e899
urltapas <ryan.bromati@hotmail.com>
2024-07-18 01:37:04 -0300

    feat: refactor UnitOfWork to use explicit interface implementation

--- src/AuthLab.Infra/UnitOfWork/UnitOfWork.cs
+++ src/AuthLab.Infra/UnitOfWork/UnitOfWork.cs
@@ -6,7 +6,12 @@ namespace AuthLab.Infra.UnitOfWork;
 public class UnitOfWork<T>(AuthLabDbContext context, IGenericRepository<T> repository) : IUnitOfWork<T> where T : class
 {
     private readonly AuthLabDbContext _context = context;
-    public IGenericRepository<T> Repository { get; set; } = repository;
+    private IGenericRepository<T> Repository { get; set; } = repository;
+
+    IGenericRepository<T> IUnitOfWork<T>.Repository()
+    {
+        return Repository;
+    }
 
     public async Task<int> SaveChangesAsync()
     {
