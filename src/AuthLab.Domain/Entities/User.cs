4af69f9c26791085212e3289ddedd0623bc120f7
urltapas <ryan.bromati@hotmail.com>
2024-07-18 02:12:58 -0300

    fix: typo in user class constructor parameter name

--- src/AuthLab.Domain/Entities/User.cs
+++ src/AuthLab.Domain/Entities/User.cs
@@ -1,8 +1,8 @@
 namespace AuthLab.Domain.Entities;
 
-public class User(string username, string pasword)
+public class User(string username, string password)
 {
-    public int Id { get; private set; }
+    public int? Id { get; }
     public string Username { get; private set; } = username;
-    public string Password { get; private set; } = pasword;
+    public string Password { get; private set; } = password;
 }
