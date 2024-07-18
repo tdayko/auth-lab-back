b0d4a069dbb30da319c9ab86d6184b7b29121a39
urltapas <ryan.bromati@hotmail.com>
2024-07-18 00:09:34 -0300

    chore: add project reference to AuthLab.Infra in AuthLab.API

--- src/AuthLab.API/Program.cs
+++ src/AuthLab.API/Program.cs
@@ -1,7 +1,11 @@
+using AuthLab.Infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddInfra();
 
 var app = builder.Build();
 
