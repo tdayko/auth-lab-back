222a3da992384d26b96c9e336a8b0516238002de
urltapas <ryan.bromati@hotmail.com>
2024-07-18 19:07:01 -0300

    refactor: add email property to user class

--- src/AuthLab.Domain/Entities/User.cs
+++ src/AuthLab.Domain/Entities/User.cs
@@ -1,8 +1,9 @@
 namespace AuthLab.Domain.Entities;
 
-public class User(string username, string password)
+public class User(string email, string username, string password)
 {
-    public int? Id { get; }
+    public int Id { get; }
+    public string Email { get; private set; } = email;
     public string Username { get; private set; } = username;
     public string Password { get; private set; } = password;
 }
