c8df04875bf33712c0a52b5a7fc70a7bf54cd83c
urltapas <ryan.bromati@hotmail.com>
2024-07-18 01:37:10 -0300

    #

--- src/AuthLab.Application/UnitOfWork/IUnitOfWork.cs
+++ src/AuthLab.Application/UnitOfWork/IUnitOfWork.cs
@@ -2,6 +2,7 @@ namespace AuthLab.Application.UnitOfWork;
 
 public interface IUnitOfWork<T> where T : class
 {
+    IGenericRepository<T> Repository();
     Task<int> SaveChangesAsync();
     void Dispose();
 }
